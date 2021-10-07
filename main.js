58c50c9402312a94835eaa194589ea78ae2878fe
Santiago Patiño <santipmartinez@outlook.com>
2021-10-06 21:30:49 -0500

    feat: add condition for validate filename

--- main.js
+++ main.js
@@ -7,13 +7,14 @@ const form = document.getElementById("form-file");
 const validateFile = (e) => {
     e.preventDefault();
     const file = document.getElementById("file").files[0];
+    const fileName = file.name.split(".");
     
-    if (!VALID_FORMATS.includes(file.name.split(".")[1])) {
+    if (!VALID_FORMATS.includes(fileName[fileName.length - 1])) {
         alert(`The format is not allowed. \n Allowed formats: ${VALID_FORMATS.join(" ")}`);
         return;
     }
 
-    const fileSizeMB = file.size / Math.pow(1024,2);
+    const fileSizeMB = file.size / Math.pow(1024, 2);
 
     if (fileSizeMB > MAXIMUM_SIZE) {
         alert(`Maximum size ${MAXIMUM_SIZE} MB`);
